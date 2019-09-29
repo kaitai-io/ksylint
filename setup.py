782346f894f357712b3e934801a7def9d550f82c
Jonas Plum <git@cugu.eu>
2019-09-29 10:51:05 +0200

    Fix pypi desc

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     version='0.2.1',
     description='A linter for ksy files.',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url='http://github.com/cugu/ksylint',
     author='Jonas Plum',
     license='GPL-3.0-or-later',
