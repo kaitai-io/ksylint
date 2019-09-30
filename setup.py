044835a1e6adab67a0973a482dd1221688e88596
Jonas Plum <cugu@users.noreply.github.com>
2019-10-01 00:41:15 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ksylint',
-    version='0.2.1',
+    version='0.3.0',
     description='A linter for ksy files.',
     long_description=long_description,
     long_description_content_type="text/markdown",
